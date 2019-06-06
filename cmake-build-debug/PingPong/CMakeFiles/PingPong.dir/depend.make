733439b3e311421780f36f1e0f695b06cb3d8dd0
Thomas Watters <thomaswatters@gmail.com>
2019-06-06 09:04:36 -0400

    Removing cmake-build-debug

--- cmake-build-debug/PingPong/CMakeFiles/PingPong.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for PingPong.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/thirdparty/spdlog/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thomaswatters/workspace/PingPong/thirdparty/spdlog/tests
-# Build directory: /home/thomaswatters/workspace/PingPong/cmake-build-debug/thirdparty/spdlog/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(spdlog-utests "/home/thomaswatters/workspace/PingPong/cmake-build-debug/thirdparty/spdlog/tests/spdlog-utests")
